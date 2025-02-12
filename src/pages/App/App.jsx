89ebccf13cd1be313c28d200391690d2e87d29be
Angus <34198794+angusmc90@users.noreply.github.com>
2025-02-12 14:17:07 -0600

    Created Auth & Fortune Page. Setting up beginning structure for log-in features before creating a seperate branch for that

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
+
+// Import the following components
+import AuthPage from '../AuthPage/AuthPage';
+import FortunePage from '../FortunePage/FortunePage';
+
 // import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // const [user, setUser] = useState({});
+
   return (
     <main className="App">
       App
+    {/* { user ?
+      <FortunePage />
+      :
+      <AuthPage />
+    } */}
     </main>
   );
 }
---
+++ src/pages/FortunePage/FortunePage.jsx
@@ -0,0 +1,5 @@
+export default function FortunePage(){
+    return(
+        <h1>FortunePage</h1>
+    )
+}
