34f3e62dbdb45b12dd225ef409f6c39b3b830a6a
Angus <34198794+angusmc90@users.noreply.github.com>
2025-02-12 13:25:48 -0600

    add MERN boilerplate structure

--- server.js
+++ server.js
@@ -3,6 +3,23 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 
+// Always require and configure near the top 
+require('dotenv').config();
+// Connect to the database
+require('./config/database');
+
+// BOILERPLATE MODELS SPACE HERE 
+// Require the Mongoose models
+// const User = require('./models/user');
+// const Item = require('./models/item');
+// const Category = require('./models/category');
+// const Order = require('./models/order');
+
+// Local variables will come in handy for holding retrieved documents
+// let user, item, category, order;
+// let users, items, categories, orders;
+
+
 const app = express();
 
 app.use(logger('dev'));
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './pages/App/App';
+// commenting out below line based on notes for template setup found here https://github.com/angusmc90/SEI_NOTES/tree/main/Unit_3/2-mern-infrastructure/2.2-mern-infrastructure-part-1
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +15,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
